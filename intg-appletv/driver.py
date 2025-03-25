0492fd1d0b2b8535d84e32fbb34b84e1875cf348
Albaintor <albaintor@github.com>
2025-03-25 15:04:10 +0100

    Fixed unavailable state

--- intg-appletv/driver.py
+++ intg-appletv/driver.py
@@ -1013,7 +1013,7 @@ async def on_atv_disconnected(identifier: str) -> None:
     """Handle ATV disconnection."""
     _LOG.debug("Apple TV disconnected: %s", identifier)
     api.configured_entities.update_attributes(
-        identifier, {media_player.Attributes.STATE: media_player.States.UNAVAILABLE}
+        identifier, {media_player.Attributes.STATE: media_player.States.OFF}
     )
 
 
