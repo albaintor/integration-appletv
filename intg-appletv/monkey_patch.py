706e2de5907f54c45eb503b72aa9230e81641074
albaintor <118518828+albaintor@users.noreply.github.com>
2026-07-27 21:25:19 +0200

    Linting

--- intg-appletv/monkey_patch.py
+++ intg-appletv/monkey_patch.py
@@ -86,7 +86,10 @@ async def patched_airplay_hap_pair_setup_procedure_start_pairing(self: AirPlayHa
 
 
 async def patched_airplay_hap_pair_setup_procedure_finish_pairing(
-    self: AirPlayHapPairSetupProcedure, username: str, pin_code: int, display_name: str | None  # noqa: ARG001
+    self: AirPlayHapPairSetupProcedure,
+    username: str,
+    pin_code: int,
+    display_name: str | None,  # noqa: ARG001
 ) -> HapCredentials:
     """Finish authentication process.
 
